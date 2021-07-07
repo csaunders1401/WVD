e299979a3ac96dd135526dcae0417157500d44c9
csaunders1401 <79836880+csaunders1401@users.noreply.github.com>
2021-07-07 10:56:09 +0000

    Download and Install Apps

---
+++ Developer/2_installNotepad++.ps1
@@ -0,0 +1,18 @@
+﻿ 
+ # install Notepad++
+ write-host 'AIB Customization: Install NotePadd++'
+ $appName = 'Notepad++'
+ $drive = 'C:\'
+ New-Item -Path $drive -Name $appName  -ItemType Directory -ErrorAction SilentlyContinue
+ $LocalPath = $drive + '\' + $appName 
+ set-Location $LocalPath
+ $NotepadPlusURL = 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.1.1/npp.8.1.1.Installer.x64.exe'
+ $NotepadPlusURLexe = 'npp.8.1.1.Installer.x64.exe'
+ $outputPath = $LocalPath + '\' + $NotepadPlusURLexe
+ Invoke-WebRequest -Uri $NotepadPlusURL -OutFile $outputPath
+ write-host 'AIB Customization: Starting Install Notepad++'
+ Start-Process -FilePath $outputPath -Args "/S" -Wait
+ write-host 'AIB Customization: Finished Install Notepad++'
+ 
+ 
+ 
---
+++ Developer/3_installVSCode.ps1
@@ -0,0 +1,15 @@
+﻿ 
+ # install VSCode
+ write-host 'AIB Customization: Install VSCode'
+ $appName = 'VSCode'
+ $drive = 'C:\'
+ New-Item -Path $drive -Name $appName  -ItemType Directory -ErrorAction SilentlyContinue
+ $LocalPath = $drive + '\' + $appName 
+ set-Location $LocalPath
+ $VSCodeURL = 'https://az764295.vo.msecnd.net/stable/507ce72a4466fbb27b715c3722558bb15afa9f48/VSCodeSetup-x64-1.57.1.exe'
+ $VSCodeURLexe = 'VSCodeUserSetup-x64-1.57.1.exe'
+ $outputPath = $LocalPath + '\' + $VSCodeURLexe
+ Invoke-WebRequest -Uri $VSCodeURL -OutFile $outputPath
+ write-host 'AIB Customization: Starting Install Latest Version of VS Code'
+ Start-Process -FilePath $outputPath -Args "/VERYSILENT /NORESTART /MERGETASKS=!runcode" -Wait
+ write-host 'AIB Customization: Finished Install Latest Version of VS Code'
