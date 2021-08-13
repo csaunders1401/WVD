fa61cda942070e74612cb077403534864d18eeb1
csaunders1401 <79836880+csaunders1401@users.noreply.github.com>
2021-08-13 07:41:01 +0000

    Update InstallBGInfo.ps1

--- Developer/InstallBGInfo.ps1
+++ Developer/InstallBGInfo.ps1
@@ -9,16 +9,18 @@
  set-Location $LocalPath
  $BGInfoURL = 'https://download.sysinternals.com/files/BGInfo.zip'
  $BGInfoConfig = 'https://github.com/csaunders1401/WVD/blob/main/Developer/DEV.bgi?raw=true'
+ $BGINFOShortcut = 'https://github.com/csaunders1401/WVD/blob/main/Developer/Bginfo.lnk?raw=true'
  $BGInfoexe = 'BGInfo.zip'
  $outputPath = $LocalPath + '\' + $BGInfoexe
  Invoke-WebRequest -Uri $BGInfoURL -OutFile $outputPath
  Invoke-WebRequest -Uri $BGInfoConfig -OutFile 'C:\Program Files\BGInfo\Dev.bgi'
+ Invoke-WebRequest -Uri $BGInfoConfig -OutFile  'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\BGInfo.lnk'
  write-host 'AIB Customization: Starting Install BGInfo'
  Expand-Archive -LiteralPath 'C:\BGInfo\BGInfo.zip' -DestinationPath 'C:\Program Files\BGInfo'
 # Start-Process -FilePath $outputPath -Args "/S" -Wait
-write-host 'AIB Customization: Finished Install Notepad++'
-'@echo off
-cd
-CALL "C:\Program Files\BGInfo\bginfo.exe" "C:\Program Files\BGInfo\DEV.bgi" /timer:0 /nolicprompt' |Out-File 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\Run.bat'
-$WindowsVersion = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ReleaseId).ReleaseId
+write-host 'AIB Customization: Finished Install BGInfo'
+#'@echo off
+#cd
+#CALL "C:\Program Files\BGInfo\bginfo.exe" "C:\Program Files\BGInfo\DEV.bgi" /timer:0 /nolicprompt' |Out-File 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\Run.bat'
+#$WindowsVersion = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ReleaseId).ReleaseId
 
