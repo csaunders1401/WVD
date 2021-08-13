a8326990b0ea3b71d8870d5948041d64fa741e0c
csaunders1401 <79836880+csaunders1401@users.noreply.github.com>
2021-08-13 10:32:19 +0000

    Create DownloadAppRemove.ps1

---
+++ Developer/DownloadAppRemove.ps1
@@ -0,0 +1,3 @@
+﻿    $url = "https://raw.githubusercontent.com/csaunders1401/WVD/main/Developer/RemoveApps.xml"
+    $output = "C:\temp\RemoveApps.xml"
+    Invoke-WebRequest -Uri $url -OutFile $output
