2a0499a5ecd97c73869b58dbda120f6d64873b13
csaunders1401 <79836880+csaunders1401@users.noreply.github.com>
2021-09-10 12:47:56 +0000

    Update CustomBuild.ps1

--- Developer/CustomBuild.ps1
+++ Developer/CustomBuild.ps1
@@ -120,8 +120,8 @@
 
  #Install GIT Desktop
  write-host 'AIB Customization: Starting Install GIT for Windows'
- #Start-Process -FilePath $GIToutputPath -Args "-silent" -Wait
- Start-Process -FilePath "C:\Windows\System32\cmd.exe" -args "/c C:\build\GIT\GitHubDesktopSetup-x64.exe -s"
+ #Start-Process -FilePath $GIToutputPath -Args "-s" -Wait
+ #Start-Process -FilePath "C:\Windows\System32\cmd.exe" -args "/c C:\build\GIT\GitHubDesktopSetup-x64.exe -s"
  write-host 'AIB Customization: Finished Install GIT for Windows' 
 
  # Install Powershell 7
